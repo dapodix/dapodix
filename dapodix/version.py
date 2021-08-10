84bdc080e906e4052d0117129f556e8e2124d96e
hexatester <revolusi147id@gmail.com>
2021-08-11 01:10:59 +0700

    Bumping version from 0.1.0 to 0.2.0

--- dapodix/version.py
+++ dapodix/version.py
@@ -1,3 +1,3 @@
 from dapodik import __dapodik_version__  # NOQA
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup
 
 setup(
     name='dapodix',
-    version='0.1.0',
+    version='0.2.0',
     description='Alat bantu aplikasi Dapodik',
     author='Habib Rohman',
-    author_email="habibrohman@protonmail.com",
+    author_email="hexatester@protonmail.com",
     url='https://github.com/hexatester/dapodix',
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=["dapodix"],
     install_requires=[
